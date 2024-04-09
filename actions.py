27b89a348b361b82cf6db6c9e8adea584956a312
marianaglzm <165334817+marianaglzm@users.noreply.github.com>
2024-04-08 21:34:53 -0400

    Created actions.py
    To separate the mechanics of the game from the functions mostly related to the board, in order to have a better organization, comprehension and to make it easier for us to work on it collaborating, but also managing the aspects of the code independently without having potential problems in the process.

---
+++ actions.py
@@ -0,0 +1,11 @@
+def check_game_over(current_board):
+  pass
+
+def is_board_full(current_board):
+  pass
+
+def generate_new_tile(current_board):
+  pass
+
+def update_score(current_score, tile_value):
+  pass
