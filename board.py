5ee5f9811f13f48af2f2a12679a099f26f86284c
marianaglzm <165334817+marianaglzm@users.noreply.github.com>
2024-04-09 08:26:22 -0400

    board.py
    We made some changes to have a better development in the project

--- board.py
+++ board.py
@@ -1,6 +1,20 @@
 def create_new_board():
-  pass
+    pass
+
 def print_board(current_board):
-  pass
-def move_piece(current_board, direction):
-  pass
+    pass
+
+def move_up(current_board):
+    pass
+
+def move_down(current_board):
+    pass
+
+def move_left(current_board):
+    pass
+
+def move_right(current_board):
+    pass
+
+def check_game_over(current_board):
+    pass
