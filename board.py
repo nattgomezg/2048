fcfae6da591b474c9883844b5fa1ebead56d669e
marianaglzm <165334817+marianaglzm@users.noreply.github.com>
2024-04-16 10:11:50 -0400

    Update board.py
    We upgraded our project

--- board.py
+++ board.py
@@ -1,20 +1,25 @@
 def create_new_board():
-    pass
+    board = [["", "", "", ""],
+             ["", "", "", ""],
+             ["", "", "", ""],
+             ["", "", "", ""]]
+    return board
 
-def print_board(current_board):
-    pass
+def print_board(board):
+    print(
+        f"""
+{board[0][0]} | {board[0][1]} | {board[0][2]} | {board[0][3]}
+-----------
+{board[1][0]} | {board[1][1]} | {board[1][2]} | {board[1][3]}
+-----------
+{board[2][0]} | {board[2][1]} | {board[2][2]} | {board[2][3]}
+-----------
+{board[3][0]} | {board[3][1]} | {board[3][2]} | {board[3][3]}
+"""
+    )
 
-def move_up(current_board):
-    pass
-
-def move_down(current_board):
-    pass
-
-def move_left(current_board):
-    pass
-
-def move_right(current_board):
+def move(direction, current_board):
     pass
 
 def check_game_over(current_board):
-    pass
+    pass
