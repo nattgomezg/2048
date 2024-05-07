c3cb51c38a1d0f31f01213d9a0d6a024469912b5
marianaglzm <165334817+marianaglzm@users.noreply.github.com>
2024-05-07 11:14:03 -0400

    actions.py
    Update

--- actions.py
+++ actions.py
@@ -1,8 +1,13 @@
-def get_user_input():
-    pass
+import random
 
-def merge_tiles(current_board, direction):
-    pass
+def move(current_board, direction):
+    for col in range(3, -1, -1):
+        # print(current_board[0][col])
+        current_board[0][3] = current_board[0][col]
 
 def add_new_tile(current_board):
-    pass
+    
+    current_board[0][0] = random.choices([2, 4, 8], [9, 4, 1], k=1)[0]
+
+def check_game_over(current_board):
+    pass
