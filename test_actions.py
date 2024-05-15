033a095cc06c25f5eda803b6e626e50d443eefb4
marianaglzm <165334817+marianaglzm@users.noreply.github.com>
2024-05-15 08:59:48 -0400

    test_actions.py
    Final changes

--- test_actions.py
+++ test_actions.py
@@ -1,14 +1,33 @@
-
+# testing actions for the game
+from game.board import create_new_board
+from game.board import drop_piece
+#import the actions from the game board
+def test_drop_piece():
+    #defining the action test drop
+    board = create_new_board()
+    drop_piece(board, 0, 2)
+    #indicating where to drop it
+    assert board == [['', '', '', ''],
+                     ['', '', '', ''],
+                     ['', '', '', ''],
+                     ['', '', '', '2']]
+    
 def test_move():
+    #defining the action of test move
     board = create_new_board()
     board[0][0] = 2
+    #indicating where to move
     move("down", board)
     end_board = create_new_board()
+    #indicating that when the board ends to insert a new one
     end_board[3][0] = 2
+    #defining what the board ending is
     assert board == end_board
 
 def test_merge(board):
+    #defining the test merge action
     for row in board:
+        # making numbers merge with a loop
         for i in range(len(row) - 1):
             if row[i] == row [i + 1] and row[i] != 0:
                 row[i] *= 2
@@ -19,15 +38,3 @@ def test_merge(board):
     row[:] = new_row
 
 return board
-
-import random
-
-def add_new_tile(board):
-    import random 
-
-def place_piece(board):
-    empty_positions = [(row, col) for row in range(len(board)) for col in range(len(board[0])) if board[row][col] == 0]
-    if empty_positions:
-        row, col = random.choice(empty_positions)
-    board[row][col] = random.choices([2,4], weights=[9, 1], k=1)[0]
-    return board
