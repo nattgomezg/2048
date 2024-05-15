f29a90e3dd6500bd25a8c73a36c37a137fd4269b
marianaglzm <165334817+marianaglzm@users.noreply.github.com>
2024-05-15 09:10:38 -0400

    test_board.py
    Final chamges

--- test_board.py
+++ test_board.py
@@ -1,8 +1,20 @@
-import random 
-
-def place_piece(board):
-    empty_positions = [(row, col) for row in range(len(board)) for col in range(len(board[0])) if board[row][col] == 0]
-    if empty_positions:
-        row, col = random.choice(empty_positions)
-    board[row][col] = random.choices([2,4], weights=[9, 1], k=1)[0]
+def create_new_board():
+    board = [["", "", "", ""],
+             ["", "", "", ""],
+             ["", "", "", ""],
+             ["", "", "", ""]]
     return board
+
+def print_board(board):
+    print(
+        f"""
+{board[0][0]} | {board[0][1]} | {board[0][2]} | {board[0][3]}
+-----------
+{board[1][0]} | {board[1][1]} | {board[1][2]} | {board[1][3]}
+-----------
+{board[2][0]} | {board[2][1]} | {board[2][2]} | {board[2][3]}
+-----------
+{board[3][0]} | {board[3][1]} | {board[3][2]} | {board[3][3]}
+"""
+    )
+    #indicating the size of the board 
